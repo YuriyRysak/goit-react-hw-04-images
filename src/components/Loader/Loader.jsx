cb2dd325741eba14b1fbf13d3c758888045cd8c4
YuriyRysak <yuriy.risak@gmail.com>
2023-02-13 18:04:20 +0200

    add liberty

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,17 @@
 // import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
-import { RotatingLines } from 'react-loader-spinner';
+import { RotatingTriangles } from 'react-loader-spinner';
 import './Loader.css';
 
 export const Loader = () => {
   return (
     <div className="Loader">
-      <RotatingLines
-        strokeColor="#3f51b590"
-        strokeWidth="5"
-        animationDuration="0.75"
-        width="96"
+      <RotatingTriangles
         visible={true}
+        height="180"
+        width="180 "
+        ariaLabel="rotating-triangels-loading"
+        wrapperStyle={{}}
+        wrapperClass="rotating-triangels-wrapper"
       />
     </div>
   );
